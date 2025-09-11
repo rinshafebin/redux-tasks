801f442be278d6610378ca74b94e516ffa4b72ec
rinshafebin <rinshafebinkk12@gmail.com>
2025-09-11 10:22:30 +0530

    creted thunk folder

--- thunk/src/App.jsx
+++ thunk/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment,decrement,reset } from './slices/counterSlice'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const count = useSelector(state.counter.count)
+  const dispatch =useDispatch()
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>count{count}</h1>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <button onClick={()=>dispatch(decrement())}>-</button>
+      <button onClick={()=>dispatch(reset())}>reset</button>
+
+
+
+    </div>
   )
 }
 
